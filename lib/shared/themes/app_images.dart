e139776364dcb9de5f5f806c6d71c24e9948e622
cancio <filipe.cancio@outlook.com>
2021-06-22 01:28:53 -0300

    💄 Setting images path

---
+++ lib/shared/themes/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  static const logoFull = "assets/images/logofull.png";
+  static const logoBig = "assets/images/logobig.png";
+  static const block = "assets/images/block.png";
+  static const google = "assets/images/google.png";
+}
