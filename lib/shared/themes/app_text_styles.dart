4bdb8411f86ed76203650898ec1664eacf5db815
cancio <filipe.cancio@outlook.com>
2021-06-22 01:14:39 -0300

    💄 Update text format

---
+++ lib/shared/themes/app_text_styles.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:paguei/shared/themes/app_colors.dart';
+
+class TextStyles {
+  static final titleHome = GoogleFonts.raleway(
+    fontSize: 32,
+    fontWeight: FontWeight.w600,
+    color: AppColors.green06,
+  );
+  static final titleRegular = GoogleFonts.raleway(
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    color: AppColors.green06,
+  );
+  static final titleBoldHeading = GoogleFonts.raleway(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.green06,
+  );
+  static final titleBoldgray01 = GoogleFonts.raleway(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    color: AppColors.gray01,
+  );
+  static final titleListTile = GoogleFonts.raleway(
+    fontSize: 17,
+    fontWeight: FontWeight.w600,
+    color: AppColors.green06,
+  );
+  static final trailingRegular = GoogleFonts.raleway(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: AppColors.green06,
+  );
+  static final trailingBold = GoogleFonts.raleway(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    color: AppColors.green06,
+  );
+  static final buttongreen05 = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.green05,
+  );
+  static final buttonHeading = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.green06,
+  );
+  static final buttonGray = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.green08,
+  );
+  static final buttongray01 = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+    color: AppColors.gray01,
+  );
+  static final buttonBoldgreen05 = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.green05,
+  );
+  static final buttonBoldHeading = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.green06,
+  );
+  static final buttonBoldGray = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.green08,
+  );
+  static final buttonBoldgray01 = GoogleFonts.inter(
+    fontSize: 15,
+    fontWeight: FontWeight.w700,
+    color: AppColors.gray01,
+  );
+  static final captiongray01 = GoogleFonts.raleway(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColors.gray01,
+  );
+  static final captionShape = GoogleFonts.raleway(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColors.gray04,
+  );
+  static final captionBody = GoogleFonts.raleway(
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    color: AppColors.gray06,
+  );
+  static final captionBoldgray01 = GoogleFonts.raleway(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColors.gray01,
+  );
+  static final captionBoldShape = GoogleFonts.raleway(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColors.gray04,
+  );
+  static final captionBoldBody = GoogleFonts.raleway(
+    fontSize: 13,
+    fontWeight: FontWeight.w600,
+    color: AppColors.gray06,
+  );
+}
