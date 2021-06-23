4a877ecaa765db92072aaea58aabd89ac88e6eb8
cancio <filipe.cancio@outlook.com>
2021-06-22 23:49:55 -0300

    lesta changes on google signin

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:paguei/modules/login/login_controller.dart';
 import 'package:paguei/shared/themes/app_colors.dart';
 import 'package:paguei/shared/themes/app_images.dart';
 import 'package:paguei/shared/themes/app_text_styles.dart';
@@ -13,6 +13,7 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final controller = LoginController();
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
@@ -54,19 +55,7 @@ class _LoginPageState extends State<LoginPage> {
                       Padding(
                         padding: const EdgeInsets.only(left: 40, right: 40),
                         child: SocialLoginButton(
-                          onTap: () async {
-                            GoogleSignIn _googleSignIn = GoogleSignIn(
-                              scopes: [
-                                'email',
-                              ],
-                            );
-                            try {
-                              final response = await _googleSignIn.signIn();
-                              print(response);
-                            } catch (error) {
-                              print(error);
-                            }
-                          },
+                          onTap: controller.googleSignIn,
                         ),
                       )
                     ],
